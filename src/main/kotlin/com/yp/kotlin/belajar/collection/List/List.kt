d647f9cb7b50d9997e9205827cdda7557424b2b4
farhanyp <36284284+farhanyp@users.noreply.github.com>
2023-01-16 08:07:20 +0700

    Kotlin Collection

---
+++ src/main/kotlin/com/yp/kotlin/belajar/collection/List/List.kt
@@ -0,0 +1,29 @@
+package com.yp.kotlin.belajar.collection.List
+
+fun main(){
+
+//    Bentuk list immutable (Tetap)
+//    val list: List<Any> = listOf("Farhan", "Yudha", "Pratama", 1, 2)
+//
+//    println(list.size) // menampilkan jumlah data di list
+//    println(list.isEmpty()) // mengecek apakah list tersebut isinya kosong ?
+//    println(list.contains("Farhan")) //mengecek apakah terdapat data "Farhan" didalam list
+//    println(list.containsAll(listOf( 1, 2))) //mengecek apakah terdapat data didalam list
+//    Menampilkan seluruh isi List immutable (Tetap)
+//    for (i in list){
+//        println(i)
+//    }
+
+//    Pembuatan mutable list
+//    var mutableList: MutableList<Any> = mutableListOf()
+//
+//    mutableList.add("Farhan") // Menambahkan data pada list
+//    mutableList.add("Yudha") // Menambahkan data pada list
+//    mutableList.remove("Yudha")// Mengapus data pada list
+//    mutableList.clear()// Mengapus seluruh data pada list
+//    for (i in mutableList){
+//        println(i)
+//    }
+
+
+}
