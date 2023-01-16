524974a2e41398077203e7f9711c6450bf4b17ce
farhanyp <36284284+farhanyp@users.noreply.github.com>
2023-01-17 06:54:20 +0700

    Update Collection

---
+++ src/main/kotlin/com/yp/kotlin/belajar/collection/Collection/Collection.kt
@@ -0,0 +1,27 @@
+package com.yp.kotlin.belajar.collection.Collection
+
+// fungsi untuk menangkap semua struktur data yang turunan dari Collection
+fun <T> displayCollection(param: Collection<T>){
+    for(value in param){
+        println("$value")
+    }
+}
+
+// fungsi untuk menangkap semua struktur data yang turunan dari mutableCollection
+fun <T> displayMutableCollection(param: MutableCollection<T>){
+    for(value in param){
+        println("$value")
+    }
+}
+
+fun main (){
+
+    displayCollection(listOf(1,2,3,4,5)) // Bisa
+    displayCollection(setOf(1,2,3,4,5)) // bisa
+//  displayCollection(mapOf<>(1,2,3,4,5)) // tidak bisa karena bukan turunan dari collection
+
+    displayMutableCollection(mutableListOf(1,2,3,4,5)) // Bisa
+    displayMutableCollection(mutableSetOf(1,2,3,4,5)) // bisa
+
+
+}
